6b750b3a74bcf7c2b003aea19d2f4324de78f7ac
dpieski <dpieski@gmail.com>
2025-05-26 11:32:12 -0500

    [doc] Updating documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "pyUSPTO"
-copyright = "2025, Andrew Piechocki"
+copyright = "2025, Dunlap Codding, PC"
 author = "Andrew Piechocki"
 
 # The full version, including alpha/beta/rc tags
